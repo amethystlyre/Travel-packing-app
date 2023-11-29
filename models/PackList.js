3c8a26757536974fc8603fe4a9932e2290dbe4e9
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-30 01:49:14 +1100

    update table models

---
+++ models/PackList.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class PackList extends Model {}
+
+PackList.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        date_from: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        date_to: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        destinations: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        transports: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        climates: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        baggage_options: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'packList',
+    }
+);
+
+module.exports = PackList;
