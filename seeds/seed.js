5ed14c06ee8e8aabe15dac80c42a7d0afa6aecc1
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-30 21:26:46 +1100

    upload seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,43 +7,38 @@ const categoryData = require('./categoryData.json');
 const baggageData = require('./baggageData.json');
 const packListData = require('./packListData.json');
 
-
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    await User.bulkCreate(userData, {
+    const users = await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
 
-    for (const category of categoryData) {
-        await Category.create({
-            ...category,
-        });
-    }
+    const categories = await Category.bulkCreate(categoryData);
 
-    for (const baggage of baggageData) {
-        await Baggage.create({
-            ...baggage,
-        });
-    }
+    const bags = await Baggage.bulkCreate(baggageData);
 
-    for (const item of itemData) {
-        await Item.create({
-            ...item,
-        });
-    }
+    const items = await Item.bulkCreate(itemData);
 
-    for (const packList of packListData) {
-        await PackList.create({
-            ...packList,
-        });
-    }
+    const packLists = await PackList.bulkCreate(packListData);
 
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
+        const { id: randomItemId } =
+            items[Math.floor(Math.random() * items.length)];
+
+        const { id: randomPackListId } =
+            packLists[Math.floor(Math.random() * packLists.length)];
         
-    }
 
+        // await ItemList.create({
+        //     item_id: randomItemId,
+        //     pack_list_id: randomPackListId,
+        // }).catch((err) => {
+        //     // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
+        //     console.log(err);
+        // });
+    }
 
     process.exit(0);
 };
