58e459ee9540dba19ac27729390659a29324f421
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-12-06 21:33:39 +1100

    add item to list

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Item,PackList } = require('../../models');
+const { Item, PackList } = require('../../models');
 const isAuth = require('../../utils/auth');
 
 router.get('/', isAuth, async (req, res) => {
@@ -15,27 +15,27 @@ router.get('/', isAuth, async (req, res) => {
     }
 });
 
-//create new item into DB
-router.post('/:plid',isAuth, async (req, res) => {
+//create new item into DB and add item to packlist
+router.post('/:plid', isAuth, async (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    try {
+        let item;
+        if (req.body && req.body.name) {
+            const { name } = req.body;
+            item = await Item.create({ name });
+        } else if (req.query && req.query.itemId) {
+            item = await Item.findByPk(req.query.itemId);
+        }
 
-    try{
-    const { name } = req.body;
-    const item = await Item.create({ name });
-    const list = await PackList.findByPk(req.params.plid);
-    await list.addListOfItems(item);
+        const list = await PackList.findByPk(req.params.plid);
+        await list.addListOfItems(item);
 
-    
-
-
-    res.status(200).json(item);
-    }
-    catch(err) {
+        res.status(200).json(item);
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
-
 });
-
-
 
 module.exports = router;
