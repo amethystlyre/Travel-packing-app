10a588dbfa64c6bc0c143267646f877a1c25dcf0
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-12-08 01:04:51 +1100

    Revert "Revert "Frontend views""

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,13 +8,4 @@ document
     .querySelector('#new-list-btn')
     .addEventListener('click', newListButton);
 
-// const updateListButton = async (event) => {
-//     event.preventDefault();
 
-//     window.location.href = `/update/${id}`;
-
-// };
-
-// document
-//     .querySelector('')
-//     .addEventListener('click', updateListButton);
--- utils/app.js
+++
@@ -1,6 +0,0 @@
-app.use(function(req, res, next) {
-    res.locals.isSpecificUrl = req.url === '/dashboard/:id';
-    next();
-   });
-
-   
